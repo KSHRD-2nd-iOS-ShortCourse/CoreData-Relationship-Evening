871c2ccc22eeb6d1b2abe51868a8c2c3d1d17502
Kokpheng <yinkokpheng@yahoo.com>
2016-10-31 23:37:15 +0700

    new

--- CoreDataOneToMany/ViewController.swift
+++ CoreDataOneToMany/ViewController.swift
@@ -7,19 +7,102 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Create Context
+        let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        
+        // Create Category and Post object
+        let categoryEntity : NSEntityDescription? = NSEntityDescription.entityForName("Category", inManagedObjectContext: context)
+        
+        let postEntity : NSEntityDescription? = NSEntityDescription.entityForName("Post", inManagedObjectContext: context)
+        
+        // Check
+        if categoryEntity != nil && postEntity != nil {
+            
+            // Create Category from context
+            let category1 : Category = Category(entity: categoryEntity!, insertIntoManagedObjectContext: context)
+            category1.name = "Vidoes"
+            
+            // Create  Set<Post> for storing each post
+            var currentSet1 : Set<Post> = Set<Post>()
+            
+            // Create POST by Loop and then insert it to Set<Post>
+            for i in 1...5{
+                let currentPost : Post = Post(entity: postEntity!, insertIntoManagedObjectContext: context)
+                currentPost.name = "Vidoes Post name \(i)"
+                currentPost.postDescription = "Vidoes Post description name \(i)"
+                
+                // Insert post to Set
+                currentSet1.insert(currentPost)
+            }
+            
+            // Set Posts to Category
+            category1.posts = NSSet(set: currentSet1)
+            
+            let category2 : Category = Category(entity: categoryEntity!, insertIntoManagedObjectContext: context)
+            category2.name = "Music"
+            var currentSet2 : Set<Post> = Set<Post>()
+            
+            for i in 1...5{
+                let currentPost : Post = Post(entity: postEntity!, insertIntoManagedObjectContext: context)
+                currentPost.name = "Music Post name \(i)"
+                currentPost.postDescription = "Music Post description name \(i)"
+                currentSet2.insert(currentPost)
+            }
+            
+            category2.posts = NSSet(set: currentSet2)
+            
+            let category3 : Category = Category(entity: categoryEntity!, insertIntoManagedObjectContext: context)
+            category3.name = "Book"
+            var currentSet3 : Set<Post> = Set<Post>()
+            
+            for i in 1...5{
+                let currentPost : Post = Post(entity: postEntity!, insertIntoManagedObjectContext: context)
+                currentPost.name = "Book Post name \(i)"
+                currentPost.postDescription = "Book Post description name \(i)"
+                currentSet3.insert(currentPost)
+            }
+            
+            category3.posts = NSSet(set: currentSet3)
+        }
+        
+        // Context Save
+        do{
+            try context.save()
+        }catch {
+            print("Err")
+        }
+        
+        // Create entity
+        let nsFetch = NSFetchRequest(entityName: "Category")
+        do{
+            // Read All Category
+            if let categoryResults = try context.executeFetchRequest(nsFetch) as? [Category] {
+                
+                // Category array
+                let contents = categoryResults
+                
+                for eachCategory in contents{
+                    print(eachCategory.name!)
+                    
+                    // Read Array of Post from Category
+                    let posts : [Post] = eachCategory.posts?.allObjects as! [Post]
+                    
+                    // Get Post detail
+                    for j in posts {
+                        print(j.name!)
+                    }
+                }
+                
+            }
+        }catch{
+            print("Error")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
